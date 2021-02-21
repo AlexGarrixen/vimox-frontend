3efd5f3ab22b067fd96a5ecc9cf5bf517a6936ec
alex texis <alextexis.js@gmail.com>
2021-02-21 12:31:12 -0600

    add: styled components and base theme

---
+++ theme/index.ts
@@ -0,0 +1,33 @@
+export const theme = {
+  colors: {
+    primary: '#377DFF',
+    gray: {
+      100: '#AAB0B7',
+      200: '#858A8F',
+      300: '#2A2C34',
+      400: '#1E2029',
+      500: '#12141D',
+    },
+  },
+  screens: {
+    xs: '320px',
+    sm: '600px',
+    md: '960px',
+    lg: '1280px',
+    xl: '1920px',
+  },
+  fontSize: {
+    xs: ['0.75rem', '1.25rem'],
+    sm: ['0.875rem', '1.375rem'],
+    base: ['1rem', '1.5rem'],
+    lg: ['1.25rem', '1.75rem'],
+    xl: ['1.5rem', '2rem'],
+    '2xl': ['1.875rem', '2.375rem'],
+    '3xl': ['2.375rem', '2.875rem'],
+    '4xl': ['2.875rem', '3.375rem'],
+    '5xl': ['3.5rem', '4rem'],
+  },
+  fontFamily: {
+    sans: ['Rubik', 'sans-serif'],
+  },
+};
