a81033d4e1f8ac19e6a2bf677066aaee90b97170
alex texis <alextexis.js@gmail.com>
2021-02-26 16:46:32 -0600

    feat: latest movies home; update: remove autoplay nav slider latest series

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,13 @@ import * as React from 'react';
 import { LayoutApp } from '@components/Layout/LayoutApp';
 import { CarouselLatestSeries } from '@pageComponents/Home/CarouselLatestSeries';
 import { LatestEpisodes } from '@pageComponents/Home/LatestEpisodes';
+import { LatestMovies } from '@pageComponents/Home/LatestMovies';
 
 const Home = () => (
   <LayoutApp>
     <CarouselLatestSeries />
     <LatestEpisodes />
+    <LatestMovies />
   </LayoutApp>
 );
 
