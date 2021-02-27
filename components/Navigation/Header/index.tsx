a788ee5e6f16d21cf46f8999327487edb27bf39a
alex texis <alextexis.js@gmail.com>
2021-02-26 19:21:09 -0600

    feat: header

---
+++ components/Navigation/Header/index.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { Search } from '@components/Icon/Search';
+import { Container } from '@components/Layout/Container';
+import { HeaderBox, Nav, LinksBox, AnchorStyled, SearchButton } from './styled';
+
+const links = [
+  {
+    title: 'Inicio',
+    href: '/',
+  },
+  {
+    title: 'Directorio',
+    href: '/directory',
+  },
+];
+
+export const Header = () => {
+  const router = useRouter();
+
+  return (
+    <HeaderBox>
+      <Container>
+        <img src='/logo.png' height={30} />
+        <Nav>
+          <LinksBox>
+            {links.map(({ title, href }, idx) => (
+              <li key={idx}>
+                <Link href={href}>
+                  <AnchorStyled isActive={router.pathname === href}>
+                    {title}
+                  </AnchorStyled>
+                </Link>
+              </li>
+            ))}
+          </LinksBox>
+        </Nav>
+        <SearchButton>
+          <Search color='#fff' xl />
+        </SearchButton>
+      </Container>
+    </HeaderBox>
+  );
+};
---
+++ components/Navigation/Header/styled.tsx
@@ -0,0 +1,59 @@
+import styled, { css } from 'styled-components';
+
+type LinkProps = {
+  isActive?: boolean;
+};
+
+export const HeaderBox = styled.header`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 5;
+  padding: 12px 0;
+
+  & > div {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+`;
+
+export const Nav = styled.nav`
+  @media (min-width: ${({ theme }) => theme.screens.md}) {
+    flex: 1;
+    margin-left: 100px;
+  }
+`;
+
+export const LinksBox = styled.ul`
+  display: none;
+
+  @media (min-width: ${({ theme }) => theme.screens.md}) {
+    display: flex;
+
+    li:not(:last-child) {
+      margin-right: 20px;
+    }
+  }
+`;
+
+export const AnchorStyled = styled.a<LinkProps>`
+  color: #fff;
+  cursor: pointer;
+
+  ${({ isActive }) =>
+    isActive &&
+    css`
+      color: ${({ theme }) => theme.colors.primary};
+    `}
+`;
+
+export const SearchButton = styled.button`
+  border: none;
+  background: transparent;
+  cursor: pointer;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+`;
