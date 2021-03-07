dad1c945cea609d5cc8ee40fb4f282b3d60f0683
alex texis <alextexis.js@gmail.com>
2021-03-07 12:22:31 -0600

    add: api services of series finder

--- components/DataDisplay/SeriesFinder/Result/index.tsx
+++ components/DataDisplay/SeriesFinder/Result/index.tsx
@@ -2,14 +2,20 @@ import React from 'react';
 import { Typography } from '@components/DataDisplay/Typography';
 import { ResultBox, Thumbnail, ContentBox } from './styled';
 
-export const Result = () => (
+type ResultProps = {
+  name: string;
+  thumbnail: string;
+  totalEpisodes: number;
+};
+
+export const Result = ({ name, thumbnail, totalEpisodes }: ResultProps) => (
   <ResultBox>
-    <Thumbnail src='placeholderImage.svg' />
+    <Thumbnail src={thumbnail} />
     <ContentBox>
       <Typography as='h6' size='sm' white>
-        TITULO DE ANIME
+        {name.toUpperCase()}
       </Typography>
-      <Typography>total de episodeos</Typography>
+      <Typography>{totalEpisodes} episodeos</Typography>
     </ContentBox>
   </ResultBox>
 );
--- components/DataDisplay/SeriesFinder/index.tsx
+++ components/DataDisplay/SeriesFinder/index.tsx
@@ -1,21 +1,53 @@
 import React from 'react';
+import Link from 'next/link';
 import { Search } from '@components/Icon/Search';
 import { Grid } from '@components/Layout/Grid';
+import { Typography } from '@components/DataDisplay/Typography';
+import { useSeriesFinder } from '@hooks/useSeriesFinder';
 import { Result } from './Result';
+import { useFinder } from './hook';
 import { SeriesFinderBox, HeaderBox, TextBox, ResultsBox } from './styled';
 
-export const SeriesFinder = () => (
-  <SeriesFinderBox>
-    <HeaderBox>
-      <Search xl />
-      <TextBox placeholder='Buscar serie' />
-    </HeaderBox>
-    <ResultsBox>
-      <Grid gap={1.5} cols='1'>
-        <Result />
-        <Result />
-        <Result />
-      </Grid>
-    </ResultsBox>
-  </SeriesFinderBox>
-);
+export const SeriesFinder = () => {
+  const { value, handleChange, results, firstRequestMade, error } = useFinder();
+  const { hideSeriesFinder } = useSeriesFinder();
+
+  return (
+    <SeriesFinderBox>
+      <HeaderBox>
+        <Search xl />
+        <TextBox
+          placeholder='Buscar serie'
+          onChange={handleChange}
+          value={value}
+        />
+      </HeaderBox>
+      <ResultsBox>
+        {results.length === 0 && !firstRequestMade && (
+          <Typography size='sm'>Encuentra tu serie favorita</Typography>
+        )}
+        {results.length === 0 && firstRequestMade && (
+          <Typography size='sm'>Sin resultados</Typography>
+        )}
+        {error && (
+          <Typography size='sm'>
+            Hubo un problema al recuperar las series
+          </Typography>
+        )}
+        <Grid gap={1.5} cols='1'>
+          {results.map(({ _id, name, episodes, imageSm }) => (
+            <Link key={_id} href={`/serie/${_id}`}>
+              <a onClick={hideSeriesFinder}>
+                <Result
+                  name={name}
+                  totalEpisodes={episodes.length}
+                  thumbnail={imageSm}
+                />
+              </a>
+            </Link>
+          ))}
+        </Grid>
+      </ResultsBox>
+    </SeriesFinderBox>
+  );
+};
