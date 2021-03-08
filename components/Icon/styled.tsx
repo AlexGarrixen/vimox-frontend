b36a2a2e52cf41ee8f76dfaf8d841da8a1310d5b
alex texis <alextexis.js@gmail.com>
2021-03-08 10:09:01 -0600

    add: re-upload icon components; update: gitignore

---
+++ components/Icon/styled.tsx
@@ -0,0 +1,42 @@
+import styled, { css } from 'styled-components';
+import type { SvgProps } from './types';
+
+export const Svg = styled.svg<SvgProps>`
+  font-size: ${({ theme }) => theme.fontSize.base[0]};
+
+  ${({ margin }) =>
+    margin &&
+    css`
+      margin: ${margin};
+    `}
+
+  ${({ sm }) =>
+    sm &&
+    css`
+      font-size: ${({ theme }) => theme.fontSize.sm[0]};
+    `};
+
+  ${({ lg }) =>
+    lg &&
+    css`
+      font-size: ${({ theme }) => theme.fontSize.lg[0]};
+    `};
+
+  ${({ xl }) =>
+    xl &&
+    css`
+      font-size: ${({ theme }) => theme.fontSize.xl[0]};
+    `};
+
+  ${({ xxl }) =>
+    xxl &&
+    css`
+      font-size: ${({ theme }) => theme.fontSize['3xl'][0]};
+    `};
+
+  ${({ xxxl }) =>
+    xxxl &&
+    css`
+      font-size: ${({ theme }) => theme.fontSize['4xl'][0]};
+    `};
+`;
