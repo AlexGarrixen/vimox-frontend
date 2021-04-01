bbee06e57b49c96773c49999d9266a962cf593e4
alexTexis <alextexis.js@gmail.com>
2021-03-31 18:08:34 -0600

    feat: impementation of login service api

---
+++ pages/api/auth/login.ts
@@ -0,0 +1,56 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { setCookie } from 'nookies';
+import atob from 'atob';
+import { requestExternalServer } from '@utils/request';
+import { getError } from '@utils/getErrorAxios';
+import { PostLoginResponse } from '@globalTypes/authServices';
+
+export default async (
+  req: NextApiRequest,
+  res: NextApiResponse
+): Promise<void> => {
+  const { method, body } = req;
+
+  if (method === 'POST') {
+    try {
+      const { data } = await requestExternalServer<PostLoginResponse>(
+        '/auth/login',
+        {
+          method: 'post',
+          auth: {
+            username: body.email,
+            password: atob(body.password),
+          },
+        }
+      );
+
+      const isSecureCookie = process.env.NODE === 'production';
+
+      setCookie({ res }, 'token', data.token, {
+        path: '/',
+        sameSite: 'lax',
+        httpOnly: true,
+        secure: isSecureCookie,
+      });
+      setCookie({ res }, 'refresh-token', data.refreshToken, {
+        path: '/',
+        sameSite: 'lax',
+        httpOnly: true,
+        secure: isSecureCookie,
+      });
+      setCookie({ res }, 'user-data', JSON.stringify(data.user), {
+        path: '/',
+        sameSite: 'lax',
+        httpOnly: true,
+        secure: isSecureCookie,
+      });
+
+      res.status(201).json(data);
+    } catch (reason) {
+      const error = getError(reason);
+      res.status(error.statusCode).json(error.payload);
+    }
+  } else {
+    res.status(405).end(`Method ${method} Not Allowed`);
+  }
+};
