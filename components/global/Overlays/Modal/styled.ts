b6c36a6d8b5130a0ea88216a33d90aa2c66d220c
alexTexis <alextexis.js@gmail.com>
2021-06-19 10:03:18 -0500

    fix: remove react-modal by own modal

---
+++ components/global/Overlays/Modal/styled.ts
@@ -0,0 +1,43 @@
+import styled, { keyframes } from 'styled-components';
+import { ModalProps } from './';
+
+const animation = keyframes`
+  0% {
+    opacity: 0;
+    transform: scale(1.05)
+  }
+  85% {
+    opacity: 1
+    transform: scale(1)
+  }
+`;
+
+export const ModalContent = styled.div<
+  Pick<ModalProps, 'maxW' | 'verticalScroll'>
+>`
+  width: 100%;
+  max-width: ${({ theme }) => theme.screens.md};
+  background-color: ${({ theme }) => theme.colors.gray[400]};
+  border-radius: 8.7px;
+  overflow-x: hidden;
+  overflow-y: auto;
+  max-height: 100%;
+  animation: ${animation} 350ms;
+
+  ${({ maxW, theme }) =>
+    maxW &&
+    maxW !== 'md' &&
+    `max-width: ${
+      typeof maxW === 'number' ? `${maxW}px` : theme.screens[maxW]
+    }`};
+
+  ${({ verticalScroll }) => !verticalScroll && 'overflow-y: hidden'}
+`;
+
+export const CloseButton = styled.span`
+  background: transparent;
+  position: absolute;
+  top: 20px;
+  right: 20px;
+  cursor: pointer;
+`;
